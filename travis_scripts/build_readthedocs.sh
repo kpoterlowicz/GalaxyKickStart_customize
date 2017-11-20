d87fbf7575bd84a7457f48985dad81af26a09753
Christophe Antoniewski <drosofff@gmail.com>
2017-11-20 09:52:50 +0100

    clone the branch mkdocs in the deployment script otherwise it cannot work \!

--- travis_scripts/build_readthedocs.sh
+++ travis_scripts/build_readthedocs.sh
@@ -6,7 +6,7 @@ mkdir mkdocs_build
 cd mkdocs_build
 # Initialize gh-pages checkout
 DATE=`date`
-git clone https://github.com/ARTbio/GalaxyKickStart.git
+git clone https://github.com/ARTbio/GalaxyKickStart.git -b mkdocs
 cd GalaxyKickStart
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
